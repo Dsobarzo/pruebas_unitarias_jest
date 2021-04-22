adcc5dff001b945f0112d4c2ebeb9dff9d9914a0
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-04-22 12:10:51 -0400

    Update calculos.test.js

--- tests/calculos.test.js
+++ tests/calculos.test.js
@@ -1,3 +1,5 @@
-test("Deberia sumar 2 numeros positivos", () => {
+const { suma } = require('../controllers/calculos')
 
+test("Deberia sumar 2 numeros positivos", () => {
+    const res = suma(1, 4);
 })
